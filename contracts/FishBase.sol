564d7503fe4cc84cdd320a2ff5d1c8abfa10c295
David Nguyen <ndtuan89@gmail.com>
2018-06-22 09:31:13 +0700

    add fish base and core contract

---
+++ contracts/FishBase.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.22;
+
+/// @title Base Contract for FishWarChain. Holds all common structs, events and base variables.
+/// @author Inspius (https://www.inspius.com)
+contract FishBase {
+  constructor() {
+  }
+}
---
+++ contracts/FishCore.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.22;
+
+/// @title Base Contract for FishWarChain. Holds all functions related to the pool.
+/// @author Inspius (https://www.inspius.com)
+contract FishCore {
+  constructor() {
+  }
+}
