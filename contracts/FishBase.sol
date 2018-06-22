da82357edfc1871361e7c3fdde5bd3cc79300d3a
David Nguyen <ndtuan89@gmail.com>
2018-06-22 13:23:40 +0700

    update contract FishBase

--- contracts/FishBase.sol
+++ contracts/FishBase.sol
@@ -7,7 +7,7 @@ import "openzeppelin-solidity/contracts/payment/PullPayment.sol";
 /// @author Inspius (https://www.inspius.com)
 contract FishBase is Ownable, PullPayment {
 	
-	constructor () public {}
+	constructor() public {}
 
 	//@dev this game's round.
 	uint256 round = 0;
@@ -17,6 +17,9 @@ contract FishBase is Ownable, PullPayment {
 	
 	//@dev a round time. 15 minutes.
 	uint256 public roundTime = 15 * 60;
+	
+	//@dev total round price
+	uint256 totalRoundPrice = 0;
 
 	//@dev Base price to join this game.
 	uint256 basePrice = 0.01 ether;
@@ -25,11 +28,7 @@ contract FishBase is Ownable, PullPayment {
 	uint256 leaderBonusPrice = 0 ether;
 
 	//@dev List of all players in this round.
-    Player[] public players;
-	
-	//@dev When a Player is created, player can start joining this game's round
-    //until the round is ended. The player's value will start at 0.01 ether 
-	//and increase when a player killed a another player.
+    mapping(address => Player) internal players;
 
 	//@dev eventEndRound event is emitted whenever a admin end this round.
     event eventEndRound(
@@ -37,15 +36,38 @@ contract FishBase is Ownable, PullPayment {
         uint256 nextEndTime,
         uint256 leaderBonusPrice
     );
+	
+	//@dev When a Player is created, player can start joining this game's round
+    //until the round is ended. The player's value will start at 0.01 ether 
+	//and increase when a player killed a another player.
 	struct Player {
 		
-		//@dev the player's address
-        address playerAddress;
-		
 		//@dev the player's round
-        uint256 round;
+        uint256 playerRound;
 		
 		//@dev the player's value. It should start at 0.01 ether
-        uint256 value;
+        uint256 playerValue;
+    }
+	
+	//@dev helper to get the dev fee of amount, current is 10%
+	function getDevFee(uint256 amount) internal pure returns(uint256) {
+        return SafeMath.div(SafeMath.mul(amount, 10), 100);
+    }
+	
+	//@dev helper to get the leader bonus fee of amount, current is 20%
+	function getLeaderBonusFee(uint256 amount) internal pure returns(uint256) {
+        return SafeMath.div(SafeMath.mul(amount, 20), 100);
+    }
+
+	//@dev helper to add amount to leader bonus
+	function addLeaderBonus(uint256 amount) internal returns(uint256) {
+		leaderBonusPrice = SafeMath.add(leaderBonusPrice, amount);
+        return leaderBonusPrice;
+    }
+
+	//@dev helper to add amount to total round price
+	function addTotalRoundPrice(uint256 amount) internal returns(uint256) {
+		totalRoundPrice = SafeMath.add(totalRoundPrice, amount);
+        return totalRoundPrice;
     }
 }
--- contracts/FishCore.sol
+++ contracts/FishCore.sol
@@ -1,8 +1,42 @@
 pragma solidity ^0.4.22;
 
-/// @title Base Contract for FishWarChain. Holds all functions related to the pool.
-/// @author Inspius (https://www.inspius.com)
-contract FishCore {
-  constructor() {
-  }
+import './FishBase.sol';
+
+//@title Base Contract for FishWarChain. Holds all functions related to the game's round.
+//@author Inspius (https://www.inspius.com)
+contract FishCore is FishBase {
+	
+	constructor() public {
+	}
+	
+	//@dev create player to join this round.
+    //@return player id in the contract.
+    function createPlayer()
+        public
+        payable
+    {
+		require(msg.value == basePrice);
+		
+		//@dev cut the dev fee
+		uint256 devFee = getDevFee(msg.value);
+		asyncSend(owner, devFee);
+
+		//@dev cut the leader bonus fee to next round
+		uint256 leaderBonusFee = getLeaderBonusFee(msg.value);		
+		addLeaderBonus(leaderBonusFee);
+		
+		Player storage player = players[msg.sender];
+		
+		uint256 valueAfterFee = SafeMath.sub(msg.value, SafeMath.add(devFee, leaderBonusFee));
+		
+		//@dev reset player value for this round
+		if(player.playerRound != round) {
+			player.playerValue = 0;
+		}
+		
+		//@dev add the player value
+		player.playerValue = SafeMath.add(player.playerValue, valueAfterFee);
+		
+		addTotalRoundPrice(player.playerValue);
+    }
 }
